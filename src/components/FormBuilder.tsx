39946a1cfcd664536b3193f3718c45f40ed6179d
ninsau <gideon@hubtel.com>
2024-09-29 02:14:39 +0000

    bug: fixed issue with no outlines on light mode

--- src/components/FormBuilder.tsx
+++ src/components/FormBuilder.tsx
@@ -77,13 +77,15 @@ const FormBuilder = <T extends Record<string, any>>({
 
   const baseFieldStyles = `
   block w-full text-sm rounded-lg shadow-sm
+  border-2 transition duration-150 ease-in-out py-2 px-3 leading-6
   ${
     enableDarkMode && isDarkMode
-      ? "bg-gray-800 border-gray-600 text-gray-200 focus:ring-blue-500 focus:border-blue-500"
-      : "bg-gray-50 border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500"
+      ? "bg-gray-800 border-gray-600 text-gray-200 focus:ring-blue-500 focus:border-blue-400 focus:outline-none"
+      : "bg-white border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 focus:outline-none"
   }
-  transition duration-150 ease-in-out py-2 px-3 leading-6
+  hover:border-gray-400
 `;
+
 
   const quillStyles = `
     ${baseFieldStyles}
